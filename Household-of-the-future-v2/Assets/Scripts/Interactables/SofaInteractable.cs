cb40d59f65ec158263e08e2b37018c7e7264243b
matteogeertsema <matteo.geertsema@gmail.com>
2020-12-14 15:01:46 +0100

    Sofa interactable toegevoegd

---
+++ Household-of-the-future-v2/Assets/Scripts/Interactables/SofaInteractable.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SofaInteractable : Interactable
+{
+    public Transform onTheSofaLocation;
+    public Transform offTheSofaLocation;
+    private Player player;
+  //  private Transform locationBeforeSittingOnSofa;
+    //private Audio liftmuziekje
+
+    private bool isSittingOnTheSofa = false;
+
+
+    // Start is called before the first frame update
+    public override void OnStart()
+    {
+        player = FindObjectOfType<PlayerController>().getPlayer();
+
+    }
+
+    public override void OnActivate()
+    {
+        sitOnSofa();
+
+    }
+
+    private void sitOnSofa()
+    {
+      //  locationBeforeSittingOnSofa = player.GetComponent<Transform>();
+        player.transform.SetPositionAndRotation(onTheSofaLocation.transform.position, onTheSofaLocation.transform.rotation);
+        player.disablePlayerControls();
+
+        this.GetComponent<BoxCollider>().isTrigger = true;
+        isSittingOnTheSofa = true;
+        StartCoroutine(waitAmoutOfSeconds(3));
+
+    }
+
+    IEnumerator waitAmoutOfSeconds(int amountInSeconds)
+    {
+        for (float i = 0; i <= amountInSeconds; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        getOffSofa();
+    }
+
+    private void getOffSofa()
+    {
+
+        player.transform.SetPositionAndRotation(offTheSofaLocation.transform.position, offTheSofaLocation.transform.rotation);
+        player.enablePlayerControls();
+        isSittingOnTheSofa = false;
+    }
+
+    public override bool isActive()
+    {
+        return isSittingOnTheSofa;
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnSelect()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void OnDeselect()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
