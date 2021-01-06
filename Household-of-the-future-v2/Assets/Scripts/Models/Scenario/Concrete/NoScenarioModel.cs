c0944673413476cc5490abd08fddfc7c6773ac53
gur-cloud <71132200+gur-cloud@users.noreply.github.com>
2021-01-06 10:46:39 +0100

    Base scenarios goede settings

--- Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/NoScenarioModel.cs
+++ Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/NoScenarioModel.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoScenarioModel : ScenarioModel {
-    public NoScenarioModel() : base(3, "Huis van de toekomst tour", "Scenario/vrijrondlopen") { }
+    public NoScenarioModel() : base(2, "Huis van de toekomst tour", "Scenario/vrijrondlopen") { }
 }
