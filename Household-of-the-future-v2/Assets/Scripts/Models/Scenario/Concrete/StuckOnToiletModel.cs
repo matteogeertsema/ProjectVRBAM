5fbf8d3c3b24492ef61eca6b173100c5db396374
gur-cloud <71132200+gur-cloud@users.noreply.github.com>
2021-01-07 10:16:08 +0100

    Scenario menu settings

--- Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/StuckOnToiletModel.cs
+++ Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/StuckOnToiletModel.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StuckOnToiletModel : ScenarioModel {
-    public StuckOnToiletModel() : base(4, "Vast op toilet", "Scenario/nachtbezoek") { }
+    public StuckOnToiletModel() : base(3, "Vast op toilet", "Scenario/nachtbezoek") { }
 }
