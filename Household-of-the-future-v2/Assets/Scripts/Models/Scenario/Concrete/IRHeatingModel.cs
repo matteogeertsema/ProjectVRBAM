f6e7ee2a7b3a0335ca1a78671ba35148ec507972
gur-cloud <71132200+gur-cloud@users.noreply.github.com>
2021-01-18 14:12:59 +0100

    infrarood verwarming ipv infrared heating

--- Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/IRHeatingModel.cs
+++ Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/IRHeatingModel.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IRHeatingModel : ScenarioModel {
-    public IRHeatingModel() : base(1, "Infrared Heating", "Scenario/IR-scenario") { }
+    public IRHeatingModel() : base(1, "Infrarood verwarming", "Scenario/IR-scenario") { }
 }
