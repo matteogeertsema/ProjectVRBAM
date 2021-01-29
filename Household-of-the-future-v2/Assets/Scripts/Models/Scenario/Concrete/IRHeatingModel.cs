f83c8b5d3d5381b6531cffab1970b6cf1a3d497d
gur-cloud <71132200+gur-cloud@users.noreply.github.com>
2021-01-29 14:38:15 +0100

    laatste dingen

--- Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/IRHeatingModel.cs
+++ Household-of-the-future-v2/Assets/Scripts/Models/Scenario/Concrete/IRHeatingModel.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IRHeatingModel : ScenarioModel {
-    public IRHeatingModel() : base(1, "Infrarood verwarming", "Scenario/IR-scenario") { }
+    public IRHeatingModel() : base(1, "Infraroodverwarming", "Scenario/IR-scenario") { }
 }
